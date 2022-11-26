338d830ee8a4f05dc70ae87dc88f4e643749b9e4
Charles Nicholson <charles.nicholson@gmail.com>
2022-11-26 16:55:30 -0500

    secure no warnings

--- make-win.bat
+++ make-win.bat
@@ -1,4 +1,4 @@
-cl.exe /std:c++20 /W4 /WX /EHsc /DNANOLOG_HOST_TOOL=1 ^
+cl.exe /std:c++20 /W4 /WX /EHsc /D_CRT_SECURE_NO_WARNINGS /DNANOLOG_HOST_TOOL=1 ^
     nanolog.c ^
     unclog/args.cc ^
     unclog/elf.cc ^
