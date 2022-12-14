a9a5d853f0cd34c5e204003a49dc00547481e098
Charles Nicholson <charles.nicholson@gmail.com>
2022-12-14 08:21:15 -0500

    unit tests on windows (#15)

--- make-win.bat
+++ make-win.bat
@@ -10,3 +10,13 @@ cl.exe /nologo /std:c++20 /W4 /WX /Osy /GL /EHsc ^
     unclog/thumb2_inst.cc ^
     unclog/unclog.cc ^
     /link /out:unclog.exe || exit /b 1
+
+cl.exe /nologo /std:c++20 /W4 /WX /Osy /GL /EHsc ^
+    /D_CRT_SECURE_NO_WARNINGS ^
+    /DNANOLOG_HOST_TOOL=1 ^
+    nanolog.c ^
+    tests/unittest_main.cc ^
+    tests/test_nanolog.cc ^
+    /link /out:nanolog_unittests.exe || exit /b 1
+
+nanolog_unittests.exe || exit /b 1
