58eac9fa3c6171396c2060fd5576111d9395d67c
Charles Nicholson <charles.nicholson@gmail.com>
2022-12-16 08:54:25 -0500

    unit tests for boilerplate runtime stuff (#20)
    * unit tests for boilerplate runtime stuff
    
    * add -Wconversion to armv7 build

--- tests/test_nanolog.cc
+++ tests/test_nanolog.cc
@@ -2,5 +2,55 @@
 #include "doctest.h"
 
 TEST_CASE("nanolog_set_threshold") {
-  REQUIRE(true);
+  REQUIRE(nanolog_set_threshold(-123) == NANOLOG_RET_ERR_BAD_ARG);
+  REQUIRE(nanolog_set_threshold(NL_SEV_DEBUG) == NANOLOG_RET_SUCCESS);
+  REQUIRE(nanolog_set_threshold(NL_SEV_INFO) == NANOLOG_RET_SUCCESS);
+  REQUIRE(nanolog_set_threshold(NL_SEV_WARNING) == NANOLOG_RET_SUCCESS);
+  REQUIRE(nanolog_set_threshold(NL_SEV_ERROR) == NANOLOG_RET_SUCCESS);
+  REQUIRE(nanolog_set_threshold(NL_SEV_CRITICAL) == NANOLOG_RET_SUCCESS);
+  REQUIRE(nanolog_set_threshold(NL_SEV_ASSERT) == NANOLOG_RET_SUCCESS);
+  REQUIRE(nanolog_set_threshold(NL_SEV_ASSERT + 1) == NANOLOG_RET_ERR_BAD_ARG);
 }
+
+TEST_CASE("nanolog_get_threshold") {
+  REQUIRE(nanolog_set_threshold(NL_SEV_ASSERT) == NANOLOG_RET_SUCCESS);
+  REQUIRE(nanolog_get_threshold() == NL_SEV_ASSERT);
+  REQUIRE(nanolog_set_threshold(NL_SEV_CRITICAL) == NANOLOG_RET_SUCCESS);
+  REQUIRE(nanolog_get_threshold() == NL_SEV_CRITICAL);
+  REQUIRE(nanolog_set_threshold(NL_SEV_ERROR) == NANOLOG_RET_SUCCESS);
+  REQUIRE(nanolog_get_threshold() == NL_SEV_ERROR);
+  REQUIRE(nanolog_set_threshold(NL_SEV_WARNING) == NANOLOG_RET_SUCCESS);
+  REQUIRE(nanolog_get_threshold() == NL_SEV_WARNING);
+  REQUIRE(nanolog_set_threshold(NL_SEV_INFO) == NANOLOG_RET_SUCCESS);
+  REQUIRE(nanolog_get_threshold() == NL_SEV_INFO);
+  REQUIRE(nanolog_set_threshold(NL_SEV_DEBUG) == NANOLOG_RET_SUCCESS);
+  REQUIRE(nanolog_get_threshold() == NL_SEV_DEBUG);
+}
+
+TEST_CASE("nanolog_set_handler") {
+  static int s_calls;
+  REQUIRE(nanolog_set_handler([](void *, int, char const *, va_list) { ++s_calls; })
+          == NANOLOG_RET_SUCCESS);
+  s_calls = 0;
+  nanolog_log_sev("", NL_SEV_ASSERT);
+  REQUIRE(s_calls == 1);
+}
+
+TEST_CASE("nanolog_fmt_is_binary") {
+  int binary = 0;
+  REQUIRE(nanolog_fmt_is_binary(nullptr, nullptr) == NANOLOG_RET_ERR_BAD_ARG);
+  REQUIRE(nanolog_fmt_is_binary(nullptr, &binary) == NANOLOG_RET_ERR_BAD_ARG);
+  REQUIRE(nanolog_fmt_is_binary("hello", nullptr) == NANOLOG_RET_ERR_BAD_ARG);
+
+  // only leading 0x1F bytes count as binary
+  static_assert(NL_BINARY_LOG_MARKER == 0x1F);
+  REQUIRE(nanolog_fmt_is_binary("\x1f", &binary) == NANOLOG_RET_SUCCESS);
+  REQUIRE(binary == 1);
+  REQUIRE(nanolog_fmt_is_binary("hello", &binary) == NANOLOG_RET_SUCCESS);
+  REQUIRE(binary == 0);
+  REQUIRE(nanolog_fmt_is_binary("\x1f" "more stuff", &binary) == NANOLOG_RET_SUCCESS);
+  REQUIRE(binary == 1);
+  REQUIRE(nanolog_fmt_is_binary("stuff\x1f", &binary) == NANOLOG_RET_SUCCESS);
+  REQUIRE(binary == 0);
+}
+
