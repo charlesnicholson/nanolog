60a24c750321a9f54b93c76ba3f127f365b1a4fb
Charles Nicholson <charles.nicholson@gmail.com>
2022-12-29 08:25:06 -0500

    another attempt at windows

--- make-win.bat
+++ make-win.bat
@@ -1,12 +1,12 @@
-@echo off
+rem @echo off
 cl.exe /nologo /std:c17 /W4 /WX /Osy /EHsc /GL /c nanolog.c || exit /b 1
 
-cl.exe /nologo /std:c++20 /W4 /WX /Osy /GL /EHsc /MP ^
+cl.exe /nologo /std:c++20 /W4 /WX /Osy /GL /EHsc /MP /c ^
     /D_CRT_SECURE_NO_WARNINGS ^
     unclog/elf.cc ^
     unclog/emit.cc ^
     unclog/thumb2.cc ^
-    unclog/thumb2_inst.cc
+    unclog/thumb2_inst.cc || exit /b 1
 
 lib.exe /out:libunclog.lib *.o
 
